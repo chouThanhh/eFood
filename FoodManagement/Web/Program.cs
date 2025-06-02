4d7633e9b29cf5ebac14b431a685a00210fbe750
Châu Thanh <ksgoku2004@gmail.com>
2025-06-02 21:19:10 +0700

    Lab 2: Create Database

--- FoodManagement/Web/Program.cs
+++ FoodManagement/Web/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Web.Models.EF;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<FoodContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("FoodDb")));
 
 var app = builder.Build();
 
@@ -23,5 +28,10 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapAreaControllerRoute(
+    name: "Admin",
+    areaName: "Admin",
+    pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
